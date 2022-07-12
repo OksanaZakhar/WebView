7a29f03c1376967f21a6ba70bac45c08f73c1610
OksanaZakhar <ksusha.vel.ksusha.vel@gmail.com>
2022-07-12 14:41:53 +0300

    java

---
+++ app/src/main/java/com/ksusha/vel/webview/MainActivity.java
@@ -0,0 +1,39 @@
+package com.ksusha.vel.webview;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        String url = "https://www.google.com";
+        webView.loadUrl(url);
+
+        webView.setWebViewClient(new WebViewClient());
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
